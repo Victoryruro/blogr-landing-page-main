6ad42fc5585509c8457574e02703734d77ab2c87
Victory Diemuodeke <Victorydiemuodeke@gmail.com>
2025-07-11 01:46:46 +0100

    Blogr

---
+++ index.js
@@ -0,0 +1,22 @@
+const menu = document.querySelector('#menucontainer');
+const hamburger= document.querySelector('#hamburger');
+let image1= true;
+const accordion = document.getElementsByClassName('accordion');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('active');
+    image1=!image1;
+    hamburger.src = image1?'./images/icon-hamburger.svg' :'./images/icon-close.svg ';
+ 
+});
+
+const mapMenu = Object.entries(accordion).map(value =>{
+    // console.log(value[1].childNodes.values()); 
+    value[1].addEventListener('click', () =>{
+        value[1].classList.toggle('active')    
+        
+    })
+     
+})
+
+
